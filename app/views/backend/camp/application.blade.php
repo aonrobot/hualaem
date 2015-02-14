d134d247fa3e28d9b9c73d22383247d80d2961d7
mix5003 <mix5003@gmail.com>
2015-02-14 10:52:27 +0700

    implement search application

--- app/views/backend/camp/application.blade.php
+++ app/views/backend/camp/application.blade.php
@@ -2,6 +2,11 @@
 
 @section('title') Application for {{$camp->name}} @stop
 
+@section('css')
+@parent
+{{ HTML::style('css/bootstrap-datetimepicker.min.css') }}
+@stop
+
 @section('content')
 <div class="container">
     <div class="row">
@@ -14,12 +19,14 @@
 <form method="POST">
     <div class="container well">
         <div class="row">
-            <div class="col-md-3">
-                <input class="form-control input-sm" placeholder="Search By Username">
-            </div>
-            <div class="col-md-3">
-                <input class="form-control input-sm" placeholder="Select Date">
-            </div>
+            <form method="GET" onsubmit="return false;">
+                <div class="col-md-3">
+                    <input class="form-control input-sm" placeholder="Search By Username" id="txtSearchUser">
+                </div>
+                <div class="col-md-3">
+                    <input class="form-control input-sm" placeholder="YYYY-MM-DD"  id="txtSearchDate">
+                </div>
+            </form>
         </div>
         <br>
         <div class="row">
@@ -31,7 +38,8 @@
         </div>
         <br>
         @foreach($camp->enrolls as $enroll)
-        <div class="row well">
+        <div class="row well row-data" data-name="{{ $enroll->user->firstname_th }} {{ $enroll->user->lastname_th }}" 
+             data-date="{{ $enroll->created_at->format('Y-m-d') }}">
             <div class="col-md-12">
                 <div class="checkbox">
                     <label>
@@ -65,33 +73,36 @@
 
 <!-- Modal -->
 <div class="modal fade" id="modalFieldData" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="myModalLabel"></h4>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-      </div>
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="myModalLabel"></h4>
+            </div>
+            <div class="modal-body">
+                ...
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+        </div>
     </div>
-  </div>
 </div>
 @stop
 
 @section('js_foot')
 @parent
+{{ HTML::script('js/moment.min.js') }}
+{{ HTML::script('js/bootstrap-datetimepicker.min.js') }}
+
 <script>
     (function () {
         var modal = $('#modalFieldData');
-        $('.btn-field-data').click(function(){
+        $('.btn-field-data').click(function () {
             var obj = $(this);
             modal.find('#myModalLabel').text(obj.data('name'));
             modal.find('.modal-body').text('Please wait....');
-            modal.find('.modal-body').load("{{ URL::route('ajax.admin.camp.camp_fields') }}/"+obj.data('id'));
+            modal.find('.modal-body').load("{{ URL::route('ajax.admin.camp.camp_fields') }}/" + obj.data('id'));
             modal.modal('show');
         });
 
@@ -106,6 +117,36 @@
         $('#btnSelectApproved').click(function () {
             $('.chk-select[data-status=APPROVED]').prop('checked', true);
         });
+
+        function filter() {
+            var name = $('#txtSearchUser').val();
+            var date = $('#txtSearchDate').val();
+
+            $('.row-data').each(function () {
+                var pass = true;
+                if (name !== '' && $(this).data('name').indexOf(name) === -1) {
+                    pass = false;
+                }
+                if (date !== '' && $(this).data('date') !== date) {
+                    pass = false;
+                }
+
+
+                if (pass) {
+                    $(this).show();
+                } else {
+                    $(this).hide();
+                }
+            });
+
+        }
+
+        $('#txtSearchUser').keyup(filter);
+
+        $('#txtSearchDate').datetimepicker({
+            format: 'YYYY-MM-DD'
+        });
+        $('#txtSearchDate').on('dp.change', filter);
     })();
 
 </script>
